4cfd6f93a1825d05122bd8fa30f1f93efe238829
Syed Rehman <srehman44@gmail.com>
2022-04-23 13:33:56 -0500

    initial commit8

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,63 +11,85 @@ const Navbar = () => {
   };
 
   return (
-    // <nav
-    //   className="navbar navbar-expand-sm navbar-light bg-white sticky-top"
-    //   // className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark"
-    //   id="navigation"
-    // >
-    //   <div className="container-fluid">
-    //     <img src={brand} width="100" />
-    //     <button
-    //       className="navbar-toggler"
-    //       type="button"
-    //       data-bs-toggle="collapse"
-    //       data-bs-target="#navbarSupportedContent"
-    //       aria-controls="navbarSupportedContent"
-    //       aria-expanded="false"
-    //       aria-label="Toggle navigation"
-    //       id="toggle-button"
-    //     >
-    //       <span className="navbar-toggler-icon"></span>
-    //     </button>
-    //     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    //       <ul className="navbar-nav ms-auto mb-2 mb-lg-0 fs-5 fw-bolder">
-    //         <li className="nav-item">
-    //           <a
-    //             className="nav-link active"
-    //             aria-current="page"
-    //             href="#"
-    //             // onclick="$('#toggle-button').click()"
-    //           >
-    //             Home
-    //           </a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a className="nav-link active" aria-current="page" href="#about">
-    //             About
-    //           </a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a className="nav-link active" href="#skills" id="test1">
-    //             My Skills
-    //           </a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a className="nav-link active" href="#work" id="test2">
-    //             Work
-    //           </a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a className="nav-link active" href="#contact">
-    //             Contact
-    //           </a>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   </div>
-    // </nav>
+    <nav
+      className="navbar navbar-expand-sm navbar-light bg-white sticky-top"
+      // className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark"
+      id="navigation"
+    >
+      <div className="container-fluid">
+        <img src={brand} width="100" />
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          id="toggle-button"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 fs-5 fw-bolder">
+            <li className="nav-item">
+              <a
+                className="nav-link active"
+                aria-current="page"
+                href="#"
+                // onclick="$('#toggle-button').click()"
+              >
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#about">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#skills" id="test1">
+                My Skills
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#work" id="test2">
+                Work
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 
-    <nav></nav>
+    // <header>
+    //   <img className="logo" src="" width="100"></img>
+    //   <nav>
+    //     <ul className="nav__links">
+    //       <li>
+    //         <a href="#">Home</a>
+    //       </li>
+    //       <li>
+    //         <a href="#about">About</a>
+    //       </li>
+    //       <li>
+    //         <a href="#skills">Skills</a>
+    //       </li>
+    //       <li>
+    //         <a href="#work">Work</a>
+    //       </li>
+    //       <li>
+    //         <a href="#contact">Contact</a>
+    //       </li>
+    //     </ul>
+    //   </nav>
+    //   <button>Contact</button>
+    // </header>
   );
 };
 
